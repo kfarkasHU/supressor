ab24fa065c207a96da5604e0e655a6ea78df9374
sohamar <sohamar@farkask.eu>
2022-04-30 01:53:29 +0200

    feat(styling): background animation

--- builder/src/packages/layout/page/page.ts
+++ builder/src/packages/layout/page/page.ts
@@ -1,9 +1,23 @@
 import m from "mithril";
 import { ContentComponent } from "../content/content";
 
+const PageComponentConfig = Object.freeze({
+  numberOfItems: 128
+});
+
 export class PageComponent implements m.ClassComponent {
   public view() {
+
+    const boxes = [];
+    for(let i = 0; i < PageComponentConfig.numberOfItems; i++) {
+      boxes.push(m(".background-container-items-item"));
+    }
+
     return m("div.supressor-container", [
+      m("div.background-container", [
+        m("div.background-container-items", boxes)
+      ]),
+      m("div.grey"),
       m(new ContentComponent())
     ]);
   }
